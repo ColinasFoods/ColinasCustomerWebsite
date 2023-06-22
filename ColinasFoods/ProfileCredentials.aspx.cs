9ff50de69771b93c0c98c17dcf7afd9b93a71398
Gulin <gulinisawesome@gmail.com>
2023-06-22 15:56:43 -0500

    fixing

--- ColinasFoods/ProfileCredentials.aspx.cs
+++ ColinasFoods/ProfileCredentials.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -13,11 +14,28 @@ namespace ColinasFoods
         {
 
 
-
+            if(Essentials.Password == null)
+            {
+                Response.Redirect("Login.aspx", false);
+                Context.ApplicationInstance.CompleteRequest();
+            }
             ((SiteMaster)Master).logOutVisible = false;
             ((SiteMaster)Master).HeaderTitle = "Profile Credentials";
             Title = "Profile Credentials";
+            loadAsteriskPassword();
 
+
+        }
+        protected void loadAsteriskPassword()
+        {
+            if (ColinasFoods.Essentials.UserName != null && ColinasFoods.Essentials.UserName.Length > 0)
+            {
+                int passwordLength = ColinasFoods.Essentials.Password.Length;
+                for (int i = 0; i < passwordLength; i++)
+                {
+                    passwordLabel.Text += '*';
+                }
+            }
 
         }
     }
